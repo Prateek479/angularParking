4fba039a4937707cac152aabe06205ce5547def8
Prateek <prateek479@gmail.com>
2015-11-17 14:20:12 +0530

    adding files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webappsApp
+ */
+angular.module('webappsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
